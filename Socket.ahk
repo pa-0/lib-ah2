pa-0/lib-ah2

commit 7f787554afe15a8fa5b063ffa82fa1a44579e3e2

fixed #84

Socket.ahk
@@ -1,8 +1,8 @@
 /************************************************************************
  * @description simple implementation of a socket Server and Client.
  * @author thqby
- * @date 2024/12/21
- * @version 1.0.5
+ * @date 2025/01/26
+ * @version 1.0.6
  ***********************************************************************/
 
 /**
@@ -102,7 +102,7 @@ class Socket {
 				this._define_async_methods(), _async_select(this, flags, 0)
 				return
 			}
-			_async_select(this, 0, 0), this.DeleteProp('_async_select')
+			_async_select(this.DefineProp('_async_select', { call: (*) => 0 }), 0, 0)
 			try sockets_table.Delete(this.Ptr), !sockets_table.Count && OnMessage(WM_SOCKET, On_WM_SOCKET, 0)
 			if start > -1 && !DllCall('ws2_32\ioctlsocket', 'ptr', this, 'int', FIONBIO, 'uint*', 0)
 				this.OnWrite(0)

commit 46882874a3db8e5e556ab27189eaa2f09f4a6125

fixed `StartTLS` not shaking hands (broken by 7f787554afe15a8fa5b063ffa82fa1a44579e3e2)

Socket.ahk
@@ -1,8 +1,8 @@
 /************************************************************************
  * @description simple implementation of a socket Server and Client.
  * @author thqby
- * @date 2025/01/26
- * @version 1.0.6
+ * @date 2025/02/15
+ * @version 1.0.7
  ***********************************************************************/
 
 /**
@@ -102,7 +102,9 @@ class Socket {
 				this._define_async_methods(), _async_select(this, flags, 0)
 				return
 			}
-			_async_select(this.DefineProp('_async_select', { call: (*) => 0 }), 0, 0)
+			if ObjHasOwnProp(this, '_async_select')
+				this.DefineProp('_async_select', { call: (*) => 0 })
+			_async_select(this, 0, 0)
 			try sockets_table.Delete(this.Ptr), !sockets_table.Count && OnMessage(WM_SOCKET, On_WM_SOCKET, 0)
 			if start > -1 && !DllCall('ws2_32\ioctlsocket', 'ptr', this, 'int', FIONBIO, 'uint*', 0)
 				this.OnWrite(0)